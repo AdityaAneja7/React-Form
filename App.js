2a7978670d1e5dc2760b6bc678e69b5801f7ac24
AdityaAneja7 <108070483+AdityaAneja7@users.noreply.github.com>
2022-06-23 18:28:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './components/Form';
+// import Multipleinputs from './components/Multipleinputs';
+// import List from './components/List';
+
+const App =() => {
+  return (
+    <div className='container my-3'>
+      <Form />
+      {/* <Multipleinputs /> */}
+      {/* <List /> */}
+    </div>
+  );
+}
+
+export default App;
